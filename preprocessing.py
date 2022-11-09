335204e32e22a07f02fe3e1311080f8d09715118
shreyarms <shreyar100@gmail.com>
2022-11-09 15:03:44 +0800

    Changed Logic For AQP generation. Edited Annotation output doctionary

--- preprocessing.py
+++ preprocessing.py
@@ -3,9 +3,11 @@ import psycopg2
 import yaml
 from yaml.loader import SafeLoader
 import sqlvalidator
-import sqlparse
+# import sqlparse
 from turtle import Turtle,Screen
 from math import acos
+
+
 
 class DB_Connection:
     """
@@ -75,84 +77,90 @@ class Query_Manager:
         """
         get_query_plan: pass in a query to execute query with EXPLAIN to output a query_plan
         """
-        self.disable_memoization()
-        query_plan_prefix = "EXPLAIN(ANALYZE,VERBOSE,FORMAT JSON) "
-        query_plan = self.DB_conn.execute(query_plan_prefix+ query)
+        query_plan_prefix = "EXPLAIN(VERBOSE, FORMAT JSON)"
+        query_plan = self.DB_conn.execute(query_plan_prefix + query)
         return query_plan[0][0][0]["Plan"]
-    
-    def disable_method(self, method:str):
-        """
-        disable_method: disable methods for AQP generation.
-        """
-        self.DB_conn.execute("SET enable_"+method+" = OFF;")
-
-    def enable_method(self, method:str):
-        """
-        enable_method: enable methods for AQP generation.
-        """
-        self.DB_conn.execute("SET enable_"+method+" = ON;")
-    
-    def disable_parallelism(self):
-        """
-        disable_parallelism: disables multiple workers 
-        """
-        self.DB_conn.execute("SET max_parallel_workers_per_gather = 0;")
-    
-    def enable_parallelism(self):
-        """
-        enable_parallelism: sets the number of multipe workers back to the default value, 2.
-        """
-        self.DB_conn.execute("SET max_parallel_workers_per_gather = 0;")
-    
-    def disable_memoization(self):
-        """
-        disable_memoization: removes cache use
-        """
-        self.DB_conn.execute("SET enable_memoize = OFF;")
-    
-
+        
     def get_query_tree(self,query_plan:str):
         """
         get_query_tree: uses the query plan to output a binary query tree using the QEP_Tree class.
         """
         tree = QEP_Tree(query_plan)
         return tree
+
+    def get_qep_information(self, query:str):
+        plan = self.get_query_plan(query)
+        query_tree = self.get_query_tree(plan)
+        queue = []
+        queue.append(query_tree.head)
+        parallel_query = False
+        scan_methods = []
+        join_methods = []
+        while(len(queue) > 0):
+            current_node = queue.pop(0)
+            if(current_node.parallel_execution):
+                parallel_query = True
+            
+            node_type = current_node.query_result["Node Type"]
+            
+            if("Scan" in node_type and node_type not in scan_methods):
+                scan_methods.append(node_type)
+            
+            elif("Join" in node_type and node_type not in join_methods):
+                join_methods.append(node_type)
+            
+            if(current_node.left is not None):
+                queue.append(current_node.left)
+            if(current_node.right is not None):
+                queue.append(current_node.right)
+        
+        qep_information = {"parallel_query": parallel_query, "join_methods": join_methods, "scan_methods": scan_methods} 
+        return qep_information
     
-    def get_aqp(self,query,method,type):
+    def set_parallelize(self, qep_information, parallelize_config):
+        parallel_query = qep_information["parallel_query"]
+        if(parallel_query):
+            self.DB_conn.execute("SET "+parallelize_config+" = 2")
+        else:
+            self.DB_conn.execute("SET "+parallelize_config+" = 0")
+    
+    def disable_method(self, method_config:str):
         """
-        get_aqp: given a query, a method, and a type, returns the AQP using that method. 
-
-        Ex: 
-        query: query = "select * from customer C,orders O where C.c_custkey = O.o_custkey"
-        method: "nestloop"
-        type: "Join"
-
-        Output: Query Plan of AQP that uses nested loop to perform the join function. 
+        disable_method: disable methods for AQP generation.
         """
-        scan_methods = ["seqscan","bitmapscan","indexscan"]
-        join_methods = ["mergejoin", "hashjoin", "nestloop"]
-        if(type == "Scan"):
-            for m in scan_methods:
-                if(m != method):
-                    self.disable_method(m)
-            self.disable_memoization()
-            qp = self.get_query_plan(query)
-            for m in scan_methods:
-                if(m != method):
-                    self.enable_method(m)
-            return qp
-        elif (type == "Join"):
-            for m in join_methods:
-                if(m != method):
-                    self.disable_method(m)
-            self.disable_memoization()
-            qp = self.get_query_plan(query)
-            for m in join_methods:
-                if(m != method):
-                    self.enable_method(m)
-            return qp
+        self.DB_conn.execute("SET "+method_config+" = OFF;")
 
+    def enable_method(self, method_config:str):
+        """
+        enable_method: enable methods for AQP generation.
+        """
+        self.DB_conn.execute("SET "+method_config+" = ON;")
 
+    def set_aqp_parameters(self, qp_scan_params, qp_join_params, scan_params, join_params):
+        for param in scan_params.keys():
+            if(param not in qp_scan_params):
+                self.disable_method(scan_params[param])
+            else:
+                self.enable_method(scan_params[param])
+        
+        for param in join_params.keys():
+            if(param not in qp_join_params):
+                self.disable_method(join_params[param])
+            else:
+                self.enable_method(join_params[param])
+    
+    def get_num_rows(self, relation):
+        row_num = self.DB_conn.execute("SELECT COUNT(*) FROM "+ relation)
+        return row_num[0][0]
+    
+    def get_index(self, relation):
+        indexes = self.DB_conn.execute("select C.COLUMN_NAME "\
+            "FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS T "\
+            "JOIN INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE C "\
+            "ON C.CONSTRAINT_NAME=T.CONSTRAINT_NAME "\
+            "WHERE C.TABLE_NAME LIKE "+ "'{}'".format(relation) + " AND T.constraint_type LIKE 'PRIMARY KEY'")
+        return indexes[0][0]
+    
 class QEP_Node():
     """
     init: Builds a QEP Node based on query input, recursively builds children QEP Nodes based on the "Plans" field in the QEP.
@@ -186,64 +194,75 @@ class QEP_Node():
             if(key.endswith("Cond")):
                 clauses = re.findall('\({1,}(.*?)\)', query_result[key])
                 self.query_clause = self.query_clause + clauses
-            elif(key == 'Filter'):
+            elif(key == "Filter"):
                 clauses = re.findall('\({1,}(.*?)\)', query_result[key])
                 self.query_clause = self.query_clause + clauses
             elif(query_result["Node Type"].endswith("Scan") and "Relation Name" in key):
-                self.query_clause.append("Scan "+ query_result["Relation Name"])
+                self.query_clause.append(query_result["Relation Name"]+ " Scan")
         
         self.query_clause.sort()
 
-        query_result.pop("Plans",None) 
+        query_result.pop("Plans", None) 
         self.query_result = query_result
         
-    def get_aqp_cost(self, credentials, query):
+    def get_aqps(self, config, query):
         """
         get_aqp_cost: For each node:
-        1) Find Node Type 
-        2) Based on Node Type, get AQP Trees for different methods
+        1) Find Node Type (Scan/Join)
+        2) Based on Node Type, get AQP Trees for different methods 
         3) For each AQP Tree, find the Node that should be considered by comparing the query clauses
         4) Get meta data for Node. 
-        5) Return dictionary that contains the meta data of the optimal QEP and opther AEP 
+        5) Return dictionary that contains the meta data of AQPs. 
         """
-        qm = Query_Manager(credentials)         
+        qm = Query_Manager(config["Database_Credentials"])         
         cost = {}
-        scan_methods = ["seqscan","bitmapscan","indexscan"]
-        join_methods = ["mergejoin", "hashjoin", "nestloop"]
-        if("Scan" in self.query_result["Node Type"]):
-            if("Seq" in self.query_result["Node Type"]):
-                method = "seqscan"
-            elif("Bitmap" in self.query_result["Node Type"]):
-                method = "bitmapscan"
-            elif("Index" in self.query_result["Node Type"]):
-                method = "indexscan"
-            cost[method] = self.query_result
-            for m in scan_methods:
-                if(m != method):
-                    aqp_tree = qm.get_query_tree(qm.get_aqp(query, m, "Scan"))
-                    node_details = aqp_tree.get_node_with_clause(self.query_clause, self.query_result)
-                    cost[m] = node_details
-            return cost
-        elif("Join" in self.query_result["Node Type"]):
-            if("Hash" in self.query_result["Node Type"]):
-                method = "hashjoin"
-            elif("Merge" in self.query_result["Node Type"]):
-                method = "mergejoin"
-            elif("Nested Loop" in self.query_result["Node Type"]):
-                method = "nestloop"
-            cost[method] = self.query_result
-            for m in join_methods:
-                if(m != method):
-                    if(not self.parallel_execution):
-                        qm.disable_parallelism()
-                    aqp_tree = qm.get_query_tree(qm.get_aqp(query, m, "Join"))
-                    node_details = aqp_tree.get_node_with_clause(self.query_clause, self.query_result)
-                    cost[m] = node_details
-                    qm.enable_parallelism()
-            return cost
+        node_type = self.query_result["Node Type"]
+        query_plan = qm.get_qep_information(query)
+        qm.set_parallelize(query_plan, config["Supported_Query_Plan_Parameters"]["Parallelize"])
+        
+        if("Join" in node_type):
+            index = query_plan["join_methods"].index(node_type)
+            for param in config["Supported_Query_Plan_Parameters"]["Joins"]:
+                if(param != node_type):
+                    query_plan["join_methods"][index] = param
+                    qm.set_aqp_parameters(query_plan["scan_methods"], query_plan["join_methods"], config["Supported_Query_Plan_Parameters"]["Scans"], config["Supported_Query_Plan_Parameters"]["Joins"])
+                    alternate_query = qm.get_query_plan(query)
+                    alternate_query_tree = qm.get_query_tree(alternate_query)
+                    aqp_data = alternate_query_tree.get_node_with_clause(self.query_clause, self.query_result)
+                    if(aqp_data["Node Type"] != param):
+                        cost[param] = aqp_data["Node Type"]
+                    else:
+                        cost[param] = aqp_data
+        
+        elif("Scan" in node_type):
+            num_rows_after_predicate = self.query_result["Plan Rows"]
+            if(self.left is not None):
+                num_rows_before_predicate = self.left.query_result["Plan Rows"]
+            else:
+                num_rows_before_predicate = qm.get_num_rows(self.query_result["Relation Name"])
+            
+            selectivity = num_rows_after_predicate/num_rows_before_predicate
+
+            cost["selectivity"] = selectivity
+
+            #TODO: if time permits, add check to see if all columns in clauses are indexed, if they are, then it can use Index-Only 
+
+            index = query_plan["scan_methods"].index(node_type)
+            for param in config["Supported_Query_Plan_Parameters"]["Scans"]:
+                if(param != node_type):
+                    query_plan["scan_methods"][index] = param
+                    qm.set_aqp_parameters(query_plan["scan_methods"], query_plan["join_methods"], config["Supported_Query_Plan_Parameters"]["Scans"], config["Supported_Query_Plan_Parameters"]["Joins"])
+                    alternate_query = qm.get_query_plan(query)
+                    alternate_query_tree = qm.get_query_tree(alternate_query)
+                    aqp_data = alternate_query_tree.get_node_with_clause(self.query_clause, self.query_result)
+                    if(aqp_data["Node Type"] != param):
+                        cost[param] = aqp_data["Node Type"]
+                    else:
+                        cost[param] = aqp_data    
+        return cost
+            
         
 
-    
 class QEP_Tree():
     """
     init: Contains the head node of a binary query tree
@@ -254,8 +273,6 @@ class QEP_Tree():
     def get_node_with_clause(self,clause,query_result):
         """
         get_node_with_clause: Does a bfs over the binary query tree, checks if node is target node by checking clauses and QEP query result. 
-        TODO: Deal with clauses that are the same but have differing order: ['c.c_custkey = o.o_custkey'] != ['o.o_custkey = c.c_custkey']
-        TODO: Deal with scans that have a filter
         """
         queue = []
         queue.append(self.head)
@@ -269,15 +286,16 @@ class QEP_Tree():
                 if(node.right is not None):
                     queue.append(node.right)
     
+    
     def check_clauses(self,node,clause,query_result):
         """
         check_clauses: checks if the query clauses of 2 different nodes are the same. 
         """
         if(node.query_clause == clause):
-            #print("Direct Clause Match")
+            # Direct Clause Match
             return True
         elif(node.query_result["Node Type"] == "Nested Loop" and node.query_result["Output"] == query_result["Output"]):
-            #print("Nested Loop Match")
+            # Nested Loop Match
             return True
         elif("Scan" in node.query_result["Node Type"] and "Scan" in query_result["Node Type"]):
             node_scan = None
@@ -289,7 +307,7 @@ class QEP_Tree():
                 if("Scan" in clause[i]):
                     target_scan = clause[i]
             if(node_scan == target_scan):
-                #print("Scan Match")
+                # Scan Match
                 return True
         else:
             node_clauses = []
@@ -302,54 +320,54 @@ class QEP_Tree():
                 c = re.split("\s+", clause[i])
                 c.sort()
                 target_clauses.append(c)
-            #print("Reorder Match",node_clauses,target_clauses,node_clauses == target_clauses)
+            # Reorder Match 
             return node_clauses == target_clauses     
           
-def traverse_node_tree(head,credentials, query):
-    queue = []
-    queue.append(head)
-    while(len(queue) > 0):
-        node = queue.pop(0)
-        d = node.get_aqp_cost(credentials, query)
-        print(node.query_result["Node Type"],node.query_clause)
-        if(d is not None):
-            for key in d.keys():
-                print(key, d[key]["Startup Cost"] + d[key]["Total Cost"])
-        if(node.left is not None):
-            queue.append(node.left)
-        if(node.right is not None):
-            queue.append(node.right)
+# def traverse_node_tree(head,credentials, query):
+#     queue = []
+#     queue.append(head)
+#     while(len(queue) > 0):
+#         node = queue.pop(0)
+#         d = node.get_aqp_cost(credentials, query)
+#         print(node.query_result["Node Type"],node.query_clause)
+#         if(d is not None):
+#             for key in d.keys():
+#                 print(key, d[key]["Startup Cost"] + d[key]["Total Cost"])
+#         if(node.left is not None):
+#             queue.append(node.left)
+#         if(node.right is not None):
+#             queue.append(node.right)
 
-#traverse_node_tree(optimal_qep_tree.head,credentials["Database_Credentials"], query)
-
-##################################################################################################
-
-def post_order_traverse_node_tree(head,credentials, query):
+def post_order_traverse_node_tree(head,config, query):
     '''
     post_order_traverse_node_tree: meant for use in annotation, going in order of operator evaluation
     '''
     if head == None:
         return
-    post_order_traverse_node_tree(head.left,credentials, query)
-    post_order_traverse_node_tree(head.right,credentials, query)
+    post_order_traverse_node_tree(head.left,config, query)
+    post_order_traverse_node_tree(head.right,config, query)
 
-    d = head.get_aqp_cost(credentials, query)
+    d = head.get_aqps(config, query)
     if(head.query_clause != []):
-        dict_key = (head.query_result["Node Type"],head.query_clause[0])
-        tree_dict[dict_key] = []
+        dict_key = (str([head.query_result["Node Type"]]+head.query_clause))
+    else:
+        dict_key = str([head.query_result["Node Type"]])
+    
     if (d is not None):
-        for key in d.keys():
-            dict_key_entry = (key, d[key]["Startup Cost"] + d[key]["Total Cost"])
-            tree_dict[dict_key].append(dict_key_entry)
+        tree_dict[dict_key] = d
+        # for key in d.keys():
+        #     #dict_key_entry = (key, d[key]["Startup Cost"] + d[key]["Total Cost"])
+        #     #tree_dict[dict_key].append(dict_key_entry)
+        
 
 
-def post_order_wrap(head, credentials, query):
+def post_order_wrap(head, config, query):
     '''
     wrapper function for post_order_traverse_node_tree so that the results can be stored in a dictionary through recursive calls
     '''
     global tree_dict
     tree_dict = {}
-    post_order_traverse_node_tree(head,credentials,query)
+    post_order_traverse_node_tree(head,config,query)
     return tree_dict
 
     list_tree = [i for i in list_tree if i != None]
@@ -366,9 +384,13 @@ def height(root):
     return max(height(root.left), height(root.right)) + 1
 
 
+
+#Preprocessing Of Diagram - shoudl this be under another file?
+#################################################################################################
+
 DOT_DIAMETER = 30
 GENERATION_DISTANCE = 75
-def tree(turtle, d, origin,node):
+def tree(turtle,d,origin,node):
     '''
     tree: function to draw the binary operator tree using turtle
     TODO (Maybe):Using Dot diameter to draw a circle around the node
@@ -402,6 +424,8 @@ def tree(turtle, d, origin,node):
 
     tree(turtle, d - 1, right,node.right) 
     tree(turtle, d - 1, left,node.left)  
+
+
 
 
 
--- project.py
+++ project.py
@@ -3,36 +3,37 @@ from tkinter import  Tk
 import yaml
 from yaml.loader import SafeLoader
 from turtle import Turtle,Screen
-from preprocessing import Query_Manager,tree,height
+import preprocessing 
 
 '''
 Execution code from preprocessing
 TODO: clean up the printing of the tree, (does the cost need to be shown too?), embed the tree into the tkinter window
 '''
 #Database-System-Principles-Project2\credentials.yaml
-with open('C:/Users/lauka/Desktop/CZ4031 Database System Principles/Project/Project 2/Database-System-Principles-Project2/credentials.yaml') as f:
-    credentials = yaml.load(f,Loader = SafeLoader)
+with open('config.yaml') as f:
+    config = yaml.load(f,Loader = SafeLoader)
 
-# qm = Query_Manager(credentials["Database_Credentials"])
-# query = "select * FROM orders O, customer C WHERE O.o_custkey = C.c_custkey"
-# optimal_qep_tree = qm.get_query_tree(qm.get_query_plan(query))
+qm = preprocessing.Query_Manager(config["Database_Credentials"])
+query = "select * FROM orders O, customer C WHERE O.o_custkey = C.c_custkey"
+optimal_qep_tree = qm.get_query_tree(qm.get_query_plan(query))
+print(preprocessing.post_order_wrap(optimal_qep_tree.head, config, query))
 
-# screen = Screen()
+screen = Screen()
 
-# turt = Turtle()
-# turt.radians()  # to accommodate acos()
-# turt.right(1.5807)
+turt = Turtle()
+turt.radians()  # to accommodate acos()
+turt.right(1.5807)
 
-# tree(turt, height(optimal_qep_tree.head)-1, (0, 0),optimal_qep_tree.head)
+preprocessing.tree(turt, preprocessing.height(optimal_qep_tree.head)-1, (0, 0),optimal_qep_tree.head)
 
-# screen.mainloop()
+screen.mainloop()
 
 
 
 '''
 Execution code from interface
 '''
-window=Tk()
+window = Tk()
 width= window.winfo_screenwidth() / 2
 height= window.winfo_screenheight() / 2
 window.geometry("%dx%d" % (width, height))
@@ -40,3 +41,4 @@ mywin=MyWindow(window)
 window.title('Query Plan Processing')
 
 window.mainloop()
+
